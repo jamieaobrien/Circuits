6e88114dd462e7069080139c62aa0c95a04827e2
jamieaobrien <jamieobrien117@gmail.com>
2019-04-29 00:11:28 -0400

    exp 3 plot

--- Lab 9/lab9_jamie.m
+++ Lab 9/lab9_jamie.m
@@ -2,10 +2,13 @@ plot(Times,Channel1V,'.-')
 hold on
 plot(Times,Channel2V,'.-')
 xlabel('Time (s)') 
-
+ylabel('Voltage (V)')
+legend('Input wave','Output wave')
 
 %%
 plot(Times,Channel1V1,'.-')
 hold on 
 plot(Times,Channel2V1,'.-')
 xlabel('Time (s)')
+ylabel('Voltage (V)')
+legend('Input wave','Output wave')
